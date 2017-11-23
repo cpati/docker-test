145cd88696f48e2758f5c41df8c03873074952c7
cpati <chidanandapati@gmail.com>
2017-11-23 13:08:41 -0800

    5

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World 4";
+        return "Hello Docker World 5";
     }
 
     public static void main(String[] args) {
